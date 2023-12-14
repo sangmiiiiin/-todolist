f2e4ca3f7351dfa476f8b20f4c2e79ef93f41cfb
SANGMIN <kwk680037@gmail.com>
2023-12-14 23:44:22 +0900

    ✨ 로그아웃 기능

--- quiz/js/greeting.js
+++ quiz/js/greeting.js
@@ -9,8 +9,11 @@ const handleLogin = (e) => {
     e.preventDefault();
     let inputValue = formInput.value;
     formInput.value = "";
+    formContainer.classList.add("hide");
+    userName.classList.remove("hide");
     userId.push(inputValue);
     saveUserId()
+    paintId(inputValue)
 }
 
 const saveUserId = () => {
@@ -19,18 +22,28 @@ const saveUserId = () => {
 
 const paintId = (Id) => {
     userName.innerText = `${Id} 님 환영합니다!`;
+    const logOutButton = document.createElement("button");
+    userName.append(logOutButton);
+    logOutButton.innerText = "로그아웃 하기";
+    logOutButton.addEventListener("click", deleteId);
+}
+
+const deleteId = () => {
+    localStorage.clear();
+    userName.classList.add("hide");
+    formContainer.classList.remove("hide");
 }
 
 const savedId = localStorage.getItem(USERID_KEY);
+
 if(savedId === null) {
     formContainer.classList.remove("hide");
+    formContainer.addEventListener("submit", handleLogin);
 } else {
     formContainer.classList.add("hide");
     userName.classList.remove("hide");
     const parsedId = JSON.parse(savedId);
-    console.log(parsedId);
     paintId(parsedId)
-
 }
 
-formContainer.addEventListener("submit", handleLogin);
+
